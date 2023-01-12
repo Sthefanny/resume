b9ac0e70aa552f8814c5e2bd696f2c7411cc6084
Sthefanny Gonzaga <sthefannygonzaga@gmail.com>
2023-01-12 14:30:51 -0300

    initial architecture done

---
+++ lib/app/app.dart
@@ -0,0 +1,66 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:flutter_mobx/flutter_mobx.dart';
+import 'package:flutter_modular/flutter_modular.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+import '../generated/l10n.dart';
+import 'app_store.dart';
+
+
+class App extends StatefulWidget {
+  static final GlobalKey<NavigatorState> navigatorKey = GlobalKey<NavigatorState>();
+
+  @override
+  State<App> createState() => _AppState();
+}
+
+class _AppState extends State<App> {
+  late final AppStore store;
+
+  @override
+  void initState() {
+    super.initState();
+    store = Modular.get<AppStore>();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Observer(
+      builder: (_) {
+        print('locale = ${store.locale}'); //Só ta funcionando com esse print
+        return ScreenUtilInit(
+          designSize: Size(360, 640),
+          minTextAdapt: true,
+          builder: (_, __) => MaterialApp.router(
+            title: 'Cíngulo Web',
+            routeInformationParser: Modular.routeInformationParser,
+            routerDelegate: Modular.routerDelegate,
+            debugShowCheckedModeBanner: false,
+            debugShowMaterialGrid: false,
+            localizationsDelegates: [
+              S.delegate,
+              GlobalMaterialLocalizations.delegate,
+              GlobalWidgetsLocalizations.delegate,
+              GlobalCupertinoLocalizations.delegate,
+            ],
+            supportedLocales: S.delegate.supportedLocales,
+            localeResolutionCallback: (locale, supportedLocales) {
+              if (supportedLocales.contains(store.locale)) {
+                return store.locale;
+              }
+
+              return Locale('pt');
+            },
+            locale: store.locale,
+            builder: (context, widget) {
+              Modular.setNavigatorKey(App.navigatorKey);
+
+              return Builder(builder: (context) => widget!);
+            },
+          ),
+        );
+      },
+    );
+  }
+}
