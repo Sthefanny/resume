2a327c73ee94512fad8753ac8c8c94e2efdad450
Sthefanny Gonzaga <sthefannygonzaga@gmail.com>
2023-05-29 16:56:00 -0300

    url

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_modular/flutter_modular.dart';
+import 'package:url_strategy/url_strategy.dart';
 
 import 'app/app.dart';
 import 'app/app_module.dart';
 
 void main() {
+  setPathUrlStrategy();
   runApp(ModularApp(module: AppModule(), child: App()));
   // runZonedGuarded<Future<void>>(
   //   () async {
